d119fbd7e455c691bec90a0ad90bf141914d30cd
PiotrNwk <piotrnowak87@outlook.com>
2025-05-07 00:58:35 +0200

    import

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "github.com/PiotrNwk/WebDev/go/api" // Adjust this import if needed
+	"net/http"
+
+	"github.com/PiotrNwk/WebDev/tree/main/go/api"
 )
 
 func main() {
-    // Serve static files from the "static" directory
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// Serve static files from the "static" directory
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    // Route root URL to your handler
-    http.HandleFunc("/", api.Handler)
+	// Route root URL to your handler
+	http.HandleFunc("/", api.Handler)
 
-    // Start the web server
-    http.ListenAndServe(":8080", nil)
+	// Start the web server
+	http.ListenAndServe(":8080", nil)
 }

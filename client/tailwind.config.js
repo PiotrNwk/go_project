139e981519478b0aa4e5e1d6c06fcfaf69cc0744
PiotrNwk <piotrnowak87@outlook.com>
2025-05-23 08:21:07 +0200

    css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "../server/dist/index.html" // Optional: if Go templates inject classes after build
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
